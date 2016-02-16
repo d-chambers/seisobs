8ffd035f7156ae4550e5edce7878e461fe74ff3d
d-chambers <djachambeador@gmail.com>
2016-02-16 15:36:47 -0800

    0.0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
     name='seisobs',
 
-    version = '0.0.1_1',
+    version = '0.0.2',
 
     description = 'A package for converting s-files to obspy catalog objects',
     
